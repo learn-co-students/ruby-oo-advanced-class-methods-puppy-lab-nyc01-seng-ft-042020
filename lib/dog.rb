5b2f0f12e3d1039e2584e3ebbc961b9936ef6b0e
jameschjiang13 <monochrome473@gmail.com>
2020-04-22 23:08:50 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+class Dog
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        puts @@all
+    end
+
+    def save
+        
+    end
+ 
+    def name
+        @name
+    end
+
+
+
+
+end
