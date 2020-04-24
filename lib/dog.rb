06cd7bf10eccb4b0a41ab10edf3d94e326feaf98
github username <github email address>
2020-04-23 23:59:44 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,8 +4,11 @@ class Dog
     @@all = []
     def initialize(name)
         @name = name
-        @@all << self
+      save
     end
+    def save
+        @@all << self
+      end
     def self.all
         @@all
     end
